68c92580d523c778f882fa93102d849eb58a503a
jsweeb <myinfiniteholdings@gmail.com>
2025-08-27 23:31:20 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js (CommonJS)
+const { createServer } = require('http');
+const next = require('next');
+
+const dev = false; // we’re in production
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const port = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`> Next.js running on port ${port}`);
+  });
+}).catch((err) => {
+  console.error('Error starting server:', err);
+  process.exit(1);
+});
